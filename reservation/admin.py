b0c53c71fb5bcd45c19646ac9639f625a97df9c0
Bogdan Viorel Burlă <bogdanviorel13@gmail.com>
2023-07-25 10:26:59 +0000

    Add reservation app model

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,49 @@
-from django.contrib import admin
+""" This module contains the admin logic for the booking app. """
 
-# Register your models here.
+from django.contrib import admin
+from .models import Reservation, Picture
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    """
+    Allows admins a quick overview of all bookings,
+    with the ability to filter by date and time for a precise overview.
+    intended for use when seating walk-in customers on a given day or
+    making business decsions. Also allows for search by booking lead.
+
+    Containts methods to accept or decline the bookings within the dropdown.
+    """
+    list_display = ('full_name', 'date', 'time', 'guests', 'status',)
+    list_filter = ('date', 'time',)
+    search_fields = ('full_name',)
+    actions = ['accept_reservation', 'decline_reservation']
+
+    def accept_reservation(self, _request, queryset):
+        """
+        Allows bookings to be accepted from the dropdown menu in admin.
+        2 is defined as declined in the STATUS tuple found in models.py on the
+        booking directory.
+        """
+        for reservation in queryset:
+            reservation.status = 1
+            reservation.save()
+
+    def decline_reservation(self, _request, queryset):
+        """
+        Allows bookings to be declined from the dropdown menu in admin.
+        2 is defined as declined in the STATUS tuple found in models.py on the
+        booking directory.
+        """
+        for reservation in queryset:
+            reservation.status = 2
+            reservation.save()
+
+
+@admin.register(Picture)
+class PictureAdmin(admin.ModelAdmin):
+    """
+    The Admin panel for the Image model. found in the booking
+    directory under models.py
+    """
+    list_display = ('name',)
--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,85 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+# A tuple to hold the status key for the Booking model.
+STATUS = ((0, "pending"), (1, "accepted"), (2, "declined"))
+
+
+class Reservation(models.Model):
+    """
+    The model for the booking app.
+
+    Inherits the user from account sign up.
+    Stores the booking: lead, email, mobile, date, time
+    and how many guests for each individual booking.
+    Plus any special requirement notes.
+
+    Defaults the booking status to 'pending' using the above tuple.
+
+    Validation for the mobile field is handled with Django's inbuilt
+    RegexValidator.
+    """
+    # Foreign Key from the User model
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # User Info needed for Booking
+    full_name = models.CharField(max_length=300, blank=False)
+    email = models.CharField(max_length=300, blank=False)
+    # Contact Number for Booking & Validator
+    phoneNumberRegex = RegexValidator(regex=r'^\+?1?\d{9,15}$')
+    mobile = models.CharField(
+        validators=[phoneNumberRegex], max_length=15, blank=False
+        )
+    # Date of Booking
+    date = models.DateField(blank=False)
+    # Time of Booking
+    time = models.TimeField(blank=False)
+    # Special Requests for Booking
+    guests = models.PositiveIntegerField(blank=False)
+    # Booking Status - status updates handled in admin
+    requests = models.TextField(max_length=400)
+    # Number of Guests on Booking
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        """
+        Orders individual bookings by date in descending order.
+
+        Uses Django's inbuilt UniqueConstraint method to ensure
+        a user cannot make a duplicate booking for the same
+        date & time as one they already have stored in the Booking database.
+
+        If a double booking is made, the errorr raised is handled in the
+        relevant views in the booking directory under views.py.
+        """
+        ordering = ['-date']
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'date', 'time'], name='unique_booking'
+            ), ]
+
+    def __str__(self):
+        """
+        Returns the booking date and time to be used as the booking title.
+        Defining this method is reccomended by Django.
+        """
+        return f'{self.date} {self.time}'
+
+
+class Picture(models.Model):
+    """
+    The model for all images used in the restobook project.
+
+    Stores the name of the image for ease of coding.
+    Also allows admins to store the URL of where the image is hosted
+    on Cloudinary.
+    """
+    picrure = CloudinaryField('picture')
+    
+    url = models.CharField(max_length=100)
+    name = models.CharField(max_length=80, blank=False)
+
+    
