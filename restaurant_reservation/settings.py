89144e279e957ae4656c804bfba81b0d2a69b574
Bogdan Viorel Burlă <bogdanviorel13@gmail.com>
2023-07-28 08:39:41 +0000

    Arrange the imports

--- restaurant_reservation/settings.py
+++ restaurant_reservation/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
-     import env
+    import env
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
